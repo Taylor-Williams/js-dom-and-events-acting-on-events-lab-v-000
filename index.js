d698d65af9d2beeaae07f315ae0ede2ca881c3a1
Taylor Williams <taylor@thewilliams.net>
2019-02-21 05:34:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function retrieveEmployeeInformation(){
 }
 function addNewElementAsLi(){
   var node = document.createElement("LI");
-var textnode = document.createTextNode("Water");
-node.appendChild(textnode);
-document.getElementById("myList").appendChild(node);
+  var textnode = document.createTextNode("Water");
+  node.appendChild(textnode);
+  document.getElementById("myList").appendChild(node);
   document.getElementsByClassName('employee-list').append(`<li>${retrieveEmployeeInformation}</li>`)
 }
 function addNewLiOnClick(){
