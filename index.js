1399a3022770c424a025a4f40ff3a10c58fdef31
Taylor Williams <taylor@thewilliams.net>
2019-02-21 05:20:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
-const inputs = document.getElementsByTagName('input');
-
-for(var i=0; i<inputs.length; i++){
-  if(inputs[i].getAttribute('type')=='button'){
-    const subBut = inputs[i]
-  }
-}
 function retrieveEmployeeInformation(){
   return document.getElementsByName('name')
 }
