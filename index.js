ff77a36b97fe10bff4a1bbab4a5f26d196cb25f6
Taylor Williams <taylor@thewilliams.net>
2019-02-21 05:15:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+const inputs = document.getElementsByTagName('input');
+
+for(var i=0; i<inputs.length; i++){
+  if(inputs[i].getAttribute('type')=='button'){
+    const subBut = inputs[i]
+  }
+}
 function retrieveEmployeeInformation(){
 
 }
